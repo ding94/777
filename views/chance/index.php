e6cb335d022ee26909d599781a0f003507bd76f4
ding94 <dw-ding@hotmail.co.uk>
2017-06-27 12:07:37 +0800

    add chance index

--- views/chance/index.php
+++ views/chance/index.php
@@ -1,9 +1,15 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>/chance/index</h1>
+<div class = "container">
+    <div class="row">
+        <div class="col-xs-4 randomNumber">1</div>
+        <div class="col-xs-4 randomNumber">1</div>
+        <div class="col-xs-4 randomNumber">1</div>
+    </div>
+    <div class="row">
+        <button type="button" class="btn btn-default btn-lg btn-block">Press</button>
+    </div>
+</div>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+
