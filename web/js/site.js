4c17b07bca1c5f8b8f1fd82b960d3347f3335e68
ding94 <dw-ding@hotmail.co.uk>
2017-06-27 14:13:41 +0800

    change rules

--- web/js/site.js
+++ web/js/site.js
@@ -1,21 +1,25 @@
-var number= [1,2,3,4,5,6,7,8,9];
+var number= [1,2,3,4,5,7];
 function randomNumber(){
    var ansA =  number[Math.floor(Math.random()*number.length)];
    var ansB =  number[Math.floor(Math.random()*number.length)];
    var ansC =  number[Math.floor(Math.random()*number.length)];
    var result="Empty";
+   if(ansA == 7 && ansB == 7 && ansC ==7)
+   {
+        result = "First Price";
+   }
    if(ansA == ansB && ansB == ansC && ansA == ansC)
    {
-       result = "First Price";
+       result = "Second Price";
    }
    else if (ansA == ansB || ansB == ansC)
    {
-         result = "Second Price";
+        result = "Third Price";
    }
    submitData(result);
 }
 
 function submitData(result)
 {
-   
+   console.log(result);
 }
