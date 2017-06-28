8f6c4c47ad7c295f133c83e12d4a11de0b05367a
ding94 <dw-ding@hotmail.co.uk>
2017-06-28 10:19:07 +0800

    remove uselss thing

--- controllers/ChanceController.php
+++ controllers/ChanceController.php
@@ -42,76 +42,27 @@ class ChanceController extends Controller
         $model = RandomController::randomNumGen($chance);
         
        if(Yii::$app->request->isAjax){
-          $this->submitReward($model->fnum, $model->snum, $model->tnum, $chance);
+          $this->submitReward($model, $chance ,$today ,$tommorow);
        }
-        
 
-        /*
-         *find wheter random datbase create
-         *if not create one
-         */
-
-        if(empty($random))
-        {
-            $random = new Random();
-            $random->fnum = $ansA;
-            $random->snum = $ansB;
-            $random->tnum = $ansC;
-            $random->userid = Yii::$app->user->identity->id;
-            $random->chance = 1;
-            $random->save();
-        }
-
-        /*
-         *detect wheter the chance
-         *in both random and chance database changed
-         *is same or not
-         *if not get the ansA,ansB,ansC update to the random
-         */
-        if($chance->chance == $random->chance)
-        {
-            $model = $random;
-        }
-        else if($chance->chance-$random->chance === 1)
-        {
-            $random->fnum = $ansA;
-            $random->snum = $ansB;
-            $random->tnum = $ansC;
-            $random->chance = $chance->chance;
-            $random->save();
-            $model  = $random;
-        }
-
-    // {
-    //   $user = Chance::find()->where('userid = :id ',[':id' => Yii::$app->user->identity->id])->one();
-    //   if(empty($user))
-    //   {
-    //        $model = new Chance;
-    //   }
-    //
-      if(Yii::$app->request->isAjax){
-          $this->submitReward($random->fnum, $random->snum, $random->tnum, $chance);
-      }
-
->>>>>>> 64050822840cfd5f6be9fa473a19c7a876c77a65
         return $this->render('index' ,['model' =>$model]);
     }
 
-  public function submitReward($fnum,$snum,$tnum,$chance){
+  public function submitReward($model,$chance,$today,$tommorow){
 
-    if ($fnum == 7 && $snum == 7 && $tnum == 7) {
+    if ($model->fnum == 7 && $model->snum == 7 && $model->tnum == 7) {
       $reward->first = 1;
       $reward->price += 10;
       $reward->userid = Yii::$app->user->identity->id;
       $reward->save();
     }
-    else if ($fnum == $snum && $snum == $tnum && $fnum == $tnum){
+    else if ($model->fnum ==$model->snum && $model->snum == $model->tnum && $model->fnumfnum == $model->tnum){
       $reward->second = 1;
       $reward->price += 5;
       $reward->userid = Yii::$app->user->identity->id;
       $reward->save();
     }
-    else if($fnum == $snum || $snum == $tnum){
+    else if($model->fnum == $model->snum || $model->snum == $model->tnum){
       $reward->third += 1;
       $reward->price += 2;
       $reward->userid = Yii::$app->user->identity->id;
@@ -121,6 +72,11 @@ class ChanceController extends Controller
 
     }
     $chance->chance += 1;
-    Chance::updateAll(['chance' => $chance->chance ],'userid = :id' ,[':id' =>  Yii::$app->user->identity->id]);
+    $condition = [
+        'and',
+        ['between','updatetime',$today ,$tommorow],
+        ['in' ,'userid' , Yii::$app->user->identity->id],
+    ];
+    Chance::updateAll(['chance' => $chance->chance ],$condition);
   }
 }
