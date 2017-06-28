45537dc508ca95eb00db585c283de310e7d96a3f
darrensoap <darrensoap@gmail.com>
2017-06-28 14:20:11 +0800

    js remove

--- web/js/site.js
+++ web/js/site.js
@@ -1,25 +1,25 @@
 function randomNumber(){
-   var ansA = $(".a").val();
-   var ansB = $(".b").val();
-   var ansC = $(".c").val();
-
-   $('div > div[id=a]').text(ansA);
-   $('div > div[id=b]').text(ansB);
-   $('div > div[id=c]').text(ansC);
-
-   if(ansA == 7 && ansB == 7 && ansC ==7)
-   {
-
-   }
-   else if(ansA == ansB && ansB == ansC && ansA == ansC)
-   {
-
-   }
-   else if (ansA == ansB || ansB == ansC)
-   {
-
-   }
-   //submitData();
+  //  var ansA = $(".a").val();
+  //  var ansB = $(".b").val();
+  //  var ansC = $(".c").val();
+   //
+  //  $('div > div[id=a]').text(ansA);
+  //  $('div > div[id=b]').text(ansB);
+  //  $('div > div[id=c]').text(ansC);
+   //
+  //  if(ansA == 7 && ansB == 7 && ansC ==7)
+  //  {
+   //
+  //  }
+  //  else if(ansA == ansB && ansB == ansC && ansA == ansC)
+  //  {
+   //
+  //  }
+  //  else if (ansA == ansB || ansB == ansC)
+  //  {
+   //
+  //  }
+   submitData();
 }
 
 function submitData()
