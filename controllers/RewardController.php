345ff75a5c1ac5cb90a9f9cd68ad6af4ad934d56
darrensoap <darrensoap@gmail.com>
2017-06-28 11:06:14 +0800

    submit reward

--- controllers/RewardController.php
+++ controllers/RewardController.php
@@ -1,12 +1,59 @@
 <?php
 
 namespace app\controllers;
+use app\models\Reward;
+use app\models\Chance;
+use yii\web\controller;
+use Yii;
 
 class RewardController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public static function emptyReward()
+    {
+      $reward = Reward::find()->where('userid = :id',[':id' => Yii::$app->user->identity->id])->one();
+      if (empty($reward)) {
+        $reward = new Reward();
+        $reward->userid = Yii::$app->user->identity->id;
+        $reward->save();
+      }
+    }
+
+    public static function submitReward($model,$chance,$today,$tommorow)
+    {
+      $reward = Reward::find()->where('userid = :id',[':id' => Yii::$app->user->identity->id])->one();
+      if ($model->fnum == 7 && $model->snum == 7 && $model->tnum == 7) {
+        $reward->first = 1;
+        $reward->price += 10;
+        $reward->userid = Yii::$app->user->identity->id;
+        $reward->save();
+      }
+      else if ($model->fnum ==$model->snum && $model->snum == $model->tnum && $model->fnum == $model->tnum){
+        $reward->second = 1;
+        $reward->price += 5;
+        $reward->userid = Yii::$app->user->identity->id;
+        $reward->save();
+      }
+      else if($model->fnum == $model->snum || $model->snum == $model->tnum){
+        $reward->third += 1;
+        $reward->price += 2;
+        $reward->userid = Yii::$app->user->identity->id;
+        $reward->save();
+      }
+      else {
+
+      }
+      $chance->chance += 1;
+      $condition = [
+          'and',
+          ['between','updatetime',$today ,$tommorow],
+          ['in' ,'userid' , Yii::$app->user->identity->id],
+      ];
+      Chance::updateAll(['chance' => $chance->chance ],$condition);
     }
 
 }
