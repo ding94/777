1b5dad655ebec5bfe93f82a368fac08c7dc1ee9e
ding94 <dw-ding@hotmail.co.uk>
2017-06-28 11:13:27 +0800

    add some design

--- views/chance/index.php
+++ views/chance/index.php
@@ -2,6 +2,7 @@
 /* @var $this yii\web\View */
 ?>
 <div class = "container">
+    <h1>Mini Game</h1>
     <?php if(empty($model)):?>
     <div class="row">
         <div class="">Today Chance have finish</div>
@@ -12,9 +13,12 @@
         <div id ="b" class="col-xs-4 randomNumber">1</div>
         <div id ="c" class="col-xs-4 randomNumber">1</div>
     </div>
-    
+    <hr>
     <div class="row">
-        <button type="button" class="btn btn-default btn-lg btn-block" onclick="randomNumber();">Press</button>
+        <div class="col-md-4 col-md-offset-4 buttonRandom">
+             <button type="button" class="btn btn-primary btn-lg btn-block" onclick="randomNumber();">Press</button>
+        </div>
+       
     </div>
     <input type="hidden" class="a" value="<?php echo $model['fnum']?>">
     <input type="hidden" class="b" value="<?php echo $model['snum']?>">
