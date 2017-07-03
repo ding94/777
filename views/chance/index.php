771afc1aed76016b4559e932164cc65cb39615db
ding94 <dw-ding@hotmail.co.uk>
2017-07-03 10:29:31 +0800

    add rules

--- views/chance/index.php
+++ views/chance/index.php
@@ -4,16 +4,25 @@
 <div class = "container">
     <h1 class="minigame">Mini Game</h1>
     <div class="row">
+        <a class="btn btn-primary btn-lg btn-block hideButton" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Rules</a>
+        <div class="collapse" id="collapseExample">
+            <div class="well">
+                Bacon ipsum dolor amet biltong turducken pork chop leberkas flank, filet mignon shank. Shankle tenderloin filet mignon, swine prosciutto shoulder bacon jowl sirloin cupim frankfurter corned beef. Ribeye pork strip steak beef picanha boudin ball tip biltong short loin rump. Ground round short ribs sausage corned beef andouille. Cow jowl alcatra tri-tip kevin pork, picanha short ribs drumstick boudin t-bone turducken capicola brisket.
 
+                Boudin tri-tip salami ground round frankfurter shank. Kielbasa beef ribs tongue beef corned beef, kevin capicola burgdoggen tail ham hock ribeye tri-tip pork pancetta prosciutto. Chuck ribeye porchetta capicola pork chop leberkas corned beef swine tail fatback flank. Leberkas short ribs turducken tenderloin shankle ball tip, ground round turkey andouille. Cupim ribeye fatback tri-tip shoulder beef porchetta burgdoggen jowl cow pork chop chuck turkey shankle.
+            </div>
+        </div>
     </div>
     <div class="lock">
-         <?php if (empty($model[0])) :?>
+        <?php if (empty($model[0])) :?>
+        <div class="chanceValue">Chance left: 5</div>
         <div id="row ">
             <div id ="a" class="col-xs-4 randomNumber" value="1"></div>
             <div id ="b" class="col-xs-4 randomNumber" value="2"></div>
             <div id ="c" class="col-xs-4 randomNumber" value="3"></div>
         </div>
       <?php elseif($model[0]->chance == 5):?>
+       <div class="chanceValue">Chance left: <?php echo 5-$model[0]->chance ?></div>
         <div class="row ">
             <div id ="a" class="col-xs-4 randomNumber" value="<?php echo $model[0]['fnum']?>"></div>
             <div id ="b" class="col-xs-4 randomNumber" value="<?php echo $model[0]['snum']?>"></div>
@@ -25,6 +34,7 @@
         <input type="hidden" id="g" value="<?php echo $model[1]['chance']?>">
 
         <?php else :?>
+        <div class="chanceValue">Chance left: <?php echo 5-$model[0]->chance ?></div>
         <div class="row ">
             <div id ="a" class="col-xs-4 randomNumber" value="<?php echo $model[0]['fnum']?>"></div>
             <div id ="b" class="col-xs-4 randomNumber" value="<?php echo $model[0]['snum']?>"></div>
