966d1b6b66727528e05f22eb7cd2af69a0ce6588
soonws <soonws97@gmail.com>
2017-07-12 09:34:06 +0800

    nav bar game2

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -45,6 +45,7 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
         $menuItems[] = ['label' => 'Game', 'url' => ['/chance/index']];
+        $menuItems[] = ['label' => 'Game2', 'url' => ['/inbetween/index']];
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
